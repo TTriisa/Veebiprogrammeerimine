94948109712503dd7f29c1dd7bc77fbe03c55613
Teet Triisa <TTriisa@users.noreply.github.com>
2017-09-12 14:15:44 +0300

    stuffz

---
+++ esimene.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<meta charset="utf-8"/>
+        <title>Triisa Teet</title>
+    </head>
+    <body>
+		<p> See on minu leht </p>
+		<p> Vajuta pizza peale, vaata mis juhtub :)</p>
+		<?php echo "hello world :)" ?>
+    </body>
+</html>
