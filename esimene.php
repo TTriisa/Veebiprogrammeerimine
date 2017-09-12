368c23a355d9341f079e2b22bf680e16f67b7542
Teet Triisa <TTriisa@users.noreply.github.com>
2017-09-12 15:28:39 +0300

    tunnilopp 9/12

--- esimene.php
+++ esimene.php
@@ -1,12 +1,40 @@
+<?php
+	$name = [];
+	$name['first'] = "Teet";
+	$name['family'] = "Triisa";
+?>
 <!DOCTYPE html>
 <html>
     <head>
 		<meta charset="utf-8"/>
         <title>Triisa Teet</title>
+		<script>
+		window.onload = function() {
+			function updateClock() {
+				var now = new Date(), 
+					months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']; 
+					time = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds(), 
+
+					date = [now.getDate(), 
+							months[now.getMonth()],
+							now.getFullYear()].join('.');
+
+				document.getElementById('Time').innerHTML = [date, time].join(' ');
+
+				setTimeout(updateClock, 1000);
+			}
+			updateClock(); 
+		}
+        </script>
     </head>
     <body>
 		<p> See on minu leht </p>
-		<p> Vajuta pizza peale, vaata mis juhtub :)</p>
-		<?php echo "hello world :)" ?>
+		<?php 
+			echo "<p> hello world :) </p>"; 
+		?>
+		<p> Minu nimi on <?= $name['first'].' '.$name['family'];?>
+		<br><br>
+		<span> Kell on: </span><span id='Time'></span>
+		<br><br>
     </body>
 </html>
